b332b5f33ce7e23440a7477815e86068e0715dd7
Wonjae LEE <wjlee.ling@gmail.com>
2023-02-23 11:03:39 +0900

    Time: 116 ms (11.91%), Space: 19.3 MB (65.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -16,3 +16,19 @@ public:
         }
     }
 };
+/* solution
+class Solution {
+public:
+    void moveZeros(vector<int>& nums) {
+    int lastNonZeroFoundAt = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt++] = nums[i];    
+        }
+    }
+    for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+        nums[i] = 0;
+    }
+    }
+}
+*/
