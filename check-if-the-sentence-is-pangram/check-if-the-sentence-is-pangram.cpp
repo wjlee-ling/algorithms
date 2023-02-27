38dac14009e974596644fc4889d427f6b8b4f0de
Wonjae LEE <wjlee.ling@gmail.com>
2023-02-27 09:43:23 +0900

    Time: 0 ms, Memory: 6.5 MB - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        unordered_set<char> st;
+        for (char chr:sentence){
+            st.insert(chr);
+        }
+        if (st.size() == 26) return true;
+        return false;
+        
+        /*
         unordered_map<char,bool> mp;
         for (char chr:sentence) {
             mp[chr] = true;
         }
         if (mp.size() == 26) return true;
         return false;
+        */
     }
 };
