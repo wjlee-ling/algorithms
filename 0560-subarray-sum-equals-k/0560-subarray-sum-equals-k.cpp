41af6a1052b2fdf338a3d80f45252bc43e83dae7
Wonjae LEE <wjlee.ling@gmail.com>
2023-03-02 13:02:10 +0900

    Time: 116 ms (36.27%), Space: 47.2 MB (13.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -7,10 +7,12 @@ public:
         
         for (int num: nums) {
             cumsum += num; 
-            ans += counts[cumsum-k];
-            cout << counts[cumsum];
-            counts[cumsum]++;
-            cout << " -> " << counts[cumsum] << endl; 
+            ans += counts[cumsum-k]; 
+            // +=1 이 아닌 이유:
+            // cumsum이 겹친다는 것은 중간에 상쇄시키는 음수값(들)이 있다는 뜻
+            // 이전에 동일한 cumsum-k을 가졌던 인덱스들 만큼의 경우의 수
+            counts[cumsum]++; // counts에 없는 key는 자동으로 0의 value 
         } return ans;
+
     }
 };
