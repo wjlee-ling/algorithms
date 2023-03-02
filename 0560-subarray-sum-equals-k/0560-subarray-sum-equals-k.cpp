ecd0ef0fbcbd315521f9aefe8ee16c915fb485c3
Wonjae LEE <wjlee.ling@gmail.com>
2023-03-02 11:39:37 +0900

    Time: 527 ms (5.04%), Space: 47.1 MB (13.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,14 +3,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> counts;
         counts[0] = 1;
-        int ans = 0, curr = 0;
+        int cumsum = 0, ans = 0;
         
         for (int num: nums) {
-            curr += num;
-            ans += counts[curr - k];
-            counts[curr]++;
-        }
-        
-        return ans;
+            cumsum += num; 
+            ans += counts[cumsum-k];
+            cout << counts[cumsum];
+            counts[cumsum]++;
+            cout << " -> " << counts[cumsum] << endl; 
+        } return ans;
     }
 };
