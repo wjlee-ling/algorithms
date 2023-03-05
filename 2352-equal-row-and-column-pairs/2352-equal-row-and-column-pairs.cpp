e8126432b99ef1df5fff8d2fb085a90cdf06330d
Wonjae LEE <wjlee.ling@gmail.com>
2023-03-06 00:00:59 +0900

    Time: 87 ms (94.51%), Space: 34.8 MB (36.02%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,11 +1,19 @@
-class Solution:
-    def equalPairs(self, grid: List[List[int]]) -> int:
-        ans = 0
-        turned = list(map(list, zip(*grid))) # turned 90 degree
-        for col in turned:
-            if col in grid:
-                ans += grid.count(col)
-       
-        return ans
-
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        size_t n = grid.size();
+        map<vector<int>, int> counts;
+        for (auto row:grid) {
+            counts[row]++;
+        }
         
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> col = {};
+            for (int j = 0; j < n; j++) {
+                col.push_back(grid[j][i]);
+            }
+            ans += counts[col];
+        } return ans;
+    }
+};
