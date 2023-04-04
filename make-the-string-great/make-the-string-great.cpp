14fd3a19a4bd99d952b95dc33fac31eda45bbad7
Wonjae LEE <wjlee.ling@gmail.com>
2023-04-04 14:39:16 +0900

    Time: 0 ms, Memory: 6.1 MB - LeetHub

--- make-the-string-great/make-the-string-great.cpp
+++ make-the-string-great/make-the-string-great.cpp
@@ -3,17 +3,9 @@ public:
     string makeGood(string s) {
         string ans="";
         for (int i=0; i<s.size(); i++) {
-            if (ans.empty()) {
-                ans.push_back(s[i]);
-                continue;
-            } else if (abs(ans.back()-s[i]) != 32) {
-                ans.push_back(s[i]);
-                continue;
-            }
-            
-            else if (!ans.empty() && abs(ans.back()-s[i]) == 32) {
+            if (!ans.empty() && abs(ans.back()-s[i]) == 32) {
                 ans.pop_back();
-            }
+            } else ans.push_back(s[i]);
             
         }
         return ans;
