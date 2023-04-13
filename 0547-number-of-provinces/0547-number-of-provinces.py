8b0aa54ffa194a68530795e42db8fbee39d51836
Wonjae LEE <wjlee.ling@gmail.com>
2023-04-13 16:27:00 +0900

    Time: 219 ms (16.33%), Space: 15 MB (10.32%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        from collections import defaultdict
+        edges = defaultdict(set)
+        N = len(isConnected)
+        for i in range(N):
+            for j in range(N):
+                if isConnected[i][j] == 1:
+                    edges[i].add(j)
+                    edges[j].add(i)
+        
+        seen = set()
+        count = 0
+        
+        for i in range(N):
+            if i in seen:
+                continue
+            count += 1
+            stack = [i]
+            while stack:
+                curr = stack.pop()
+                seen.add(curr)
+                for _next in edges[curr]:
+                    if _next not in seen:
+                        stack.append(_next)
+            
+        return count
