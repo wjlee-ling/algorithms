65bca79fe835c8b807ac47ef281ba18d233218e3
Wonjae LEE <wjlee.ling@gmail.com>
2023-04-27 20:17:22 +0900

    Time: 210 ms (23.50%), Space: 84.1 MB (60.34%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -5,12 +5,12 @@ public:
         vector<int> ans(n);
         stack<int> st;
         for (int i = n-1; i >= 0; i--) { // 뒤에서
-            while (!st.empty() && heights[i] > st.top()) {
+            while (!st.empty() && heights[i] > st.top()) { // 오른쪽에 있는 작은 애들 봄
                 st.pop();
                 ans[i]++; // [i+1,] 에서 더 작은 것들
             }
             if (!st.empty())
-                ans[i]++; // 스택보다 작음 == 다음 것만 봄
+                ans[i]++; // 현재 있는 것이 최장신이 아님 == 최장신을 볼 수 있음 
             st.push(heights[i]);
         }
         return ans;
