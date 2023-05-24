3c11a86dac956f496d007d506f0baa87b947a53a
Wonjae LEE <wjlee.ling@gmail.com>
2023-05-24 14:43:02 +0900

    Time: 40 ms (91.84%), Space: 19 MB (18.44%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int helper(vector<int>& piles, int k, int h){
+        long total=0;
+        for (double p:piles) {
+            total += ceil(p/k);
+        }
+        return (total <= h);
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left, right=0, mid;
+        left = 1;
+        for (auto const p:piles){
+            right = max(p, right);
+        }
+        
+        while (left <= right){
+            mid = left + (right-left)/2;
+            if (helper(piles, mid, h)) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+};
